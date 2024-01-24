28a84185d0f124047a02a9bd3f57571f084d2538
Vedant Singh <vedantc252@gmail.com>
2024-01-24 17:22:05 +0530

    Push Notifications

--- chat_app/lib/screens/chat.dart
+++ chat_app/lib/screens/chat.dart
@@ -2,30 +2,54 @@ import 'package:chat_app/widgets/chat_messages.dart';
 import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifs() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setupPushNotifs();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          IconButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              icon: Icon(
-                Icons.exit_to_app,
-                color: Theme.of(context).colorScheme.primary,
-              ))
-        ],
-        title: const Text(
-          'Flutter Chat',
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                icon: Icon(
+                  Icons.exit_to_app,
+                  color: Theme.of(context).colorScheme.primary,
+                ))
+          ],
+          title: const Text(
+            'Flutter Chat',
+          ),
         ),
-      ),
-      body:Column(children: [Expanded(child: ChatMessages()),NewMessage(),],)
-    );
+        body: Column(
+          children: const [
+            Expanded(child: ChatMessages()),
+            NewMessage(),
+          ],
+        ));
   }
 }
